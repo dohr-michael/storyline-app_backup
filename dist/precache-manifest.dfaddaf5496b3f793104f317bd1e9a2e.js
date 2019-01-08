24993df306a80b5d38b4c205e5faee0b7115fd93
Michael DOHR <dohr.michael@gmail.com>
2019-01-08 09:44:40 +0100

    Dist for pages

---
+++ dist/precache-manifest.dfaddaf5496b3f793104f317bd1e9a2e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "30696aad47f84e1ee54c",
+    "url": "/js/chunk-vendors.a421de5e.js"
+  },
+  {
+    "revision": "9d851531341db651fdd6",
+    "url": "/js/characters.eb072508.js"
+  },
+  {
+    "revision": "8204f5758ceedff1a75b",
+    "url": "/js/app.6ca2f241.js"
+  },
+  {
+    "revision": "c85c0b5a0252acf32754",
+    "url": "/js/about.e9a0cf3b.js"
+  },
+  {
+    "revision": "6cbb1cb4021aada208836acafa8ad078",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d851531341db651fdd6",
+    "url": "/css/characters.0e433876.css"
+  },
+  {
+    "revision": "8204f5758ceedff1a75b",
+    "url": "/css/app.1d9864b7.css"
+  }
+];
